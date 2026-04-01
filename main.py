e1721059e5b5a9bf3ecb3a09aa3a8efbb1310f30
Jatin-Khiyani <jatinkhiyani14@gmail.com>
2026-04-01 13:07:38 +0200

    Added a very basic HTML page to connect with FASTAPI, changed person properties in accordance to HTML page needs
    Co-authored-by: Franklinpalai <Franklinpalai@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from pydantic import BaseModel
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates 
+
 
 app = FastAPI()
 
-people_db = {}
+people_db = []
 
-
+templates = Jinja2Templates(directory="templates") 
+welcome_msg = "Profile for user"
 class Person(BaseModel):
     person_id: int
     name: str
     profession : str
-    phone_no: int
-    email: str
-    summary: str
+    experience: str
+    education: str
 
 
 @app.post("/people")
 def add_person(person: Person):
-    people_db[person.person_id] = person
+    people_db.append(person.dict()) 
     return {
         "message": "Person added successfully",
         "data": person
@@ -27,8 +30,28 @@ def add_person(person: Person):
 def get_all_people():
     return people_db
 
+
+@app.get("/", response_class=HTMLResponse)
+def get_HTML(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "people": people_db,  
+            "welcome_message": welcome_msg
+        }
+    )
+
+# @app.get("/people/{person_id}/profile", response_class=HTMLResponse)
+# def get_person_profile(person_id: int):
+#     if person_id in people_db:
+#         person = people_db[person_id]
+#         return templates.TemplateResponse("profile.html", {"request": {}, "person": person})
+#     return {"error": "Person not found"}
+
 @app.get("/people/{person_id}")
 def get_person(person_id: int):
-    if person_id in people_db:
-        return people_db[person_id]
+    for person in people_db:
+        if person["person_id"] == person_id:
+            return person
     return {"error": "Person not found"}
